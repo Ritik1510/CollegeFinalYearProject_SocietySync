9a6cfc98f54b78b0b0551b408164614c07ae5a81
ritik kumar <kr.ritik1510@gmail.com>
2025-08-22 14:53:45 +0530

    migration results, successfully created and updated sql queries

---
+++ server/drizzleMigrations/0000_nice_silver_fox.sql
@@ -0,0 +1,75 @@
+CREATE TABLE "announcements" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"title" text NOT NULL,
+	"content" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"created_by" integer NOT NULL,
+	"important" boolean DEFAULT false NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "apartments" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"number" text NOT NULL,
+	"building" text NOT NULL,
+	"tenant_id" integer,
+	"owner_id" integer,
+	"rent" integer NOT NULL,
+	"status" text DEFAULT 'vacant' NOT NULL,
+	"area" integer NOT NULL,
+	"amenities" text[],
+	"last_maintenance_date" timestamp,
+	"society_name" text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "maintenance_requests" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"apartment_id" integer NOT NULL,
+	"tenant_id" integer NOT NULL,
+	"description" text NOT NULL,
+	"status" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "payments" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"apartment_id" integer NOT NULL,
+	"tenant_id" integer NOT NULL,
+	"amount" integer NOT NULL,
+	"date" timestamp NOT NULL,
+	"type" text NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"username" text NOT NULL,
+	"password" text NOT NULL,
+	"role" text NOT NULL,
+	"name" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_username_unique" UNIQUE("username")
+);
+--> statement-breakpoint
+CREATE TABLE "visitors" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"purpose" text NOT NULL,
+	"status" text NOT NULL,
+	"apartment_id" integer NOT NULL,
+	"expected_at" timestamp NOT NULL,
+	"actual_entry_at" timestamp,
+	"actual_exit_at" timestamp,
+	"approved_by" integer,
+	"contact_number" text NOT NULL,
+	"pending_approval" boolean DEFAULT false
+);
+--> statement-breakpoint
+ALTER TABLE "announcements" ADD CONSTRAINT "announcements_created_by_users_id_fk" FOREIGN KEY ("created_by") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "apartments" ADD CONSTRAINT "apartments_tenant_id_users_id_fk" FOREIGN KEY ("tenant_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "apartments" ADD CONSTRAINT "apartments_owner_id_users_id_fk" FOREIGN KEY ("owner_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "maintenance_requests" ADD CONSTRAINT "maintenance_requests_apartment_id_apartments_id_fk" FOREIGN KEY ("apartment_id") REFERENCES "public"."apartments"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "maintenance_requests" ADD CONSTRAINT "maintenance_requests_tenant_id_users_id_fk" FOREIGN KEY ("tenant_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "payments" ADD CONSTRAINT "payments_apartment_id_apartments_id_fk" FOREIGN KEY ("apartment_id") REFERENCES "public"."apartments"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "payments" ADD CONSTRAINT "payments_tenant_id_users_id_fk" FOREIGN KEY ("tenant_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "visitors" ADD CONSTRAINT "visitors_apartment_id_apartments_id_fk" FOREIGN KEY ("apartment_id") REFERENCES "public"."apartments"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "visitors" ADD CONSTRAINT "visitors_approved_by_users_id_fk" FOREIGN KEY ("approved_by") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
---
+++ server/drizzleMigrations/0001_fixing-timestamp-issue.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "maintenance_requests" RENAME COLUMN "created_at" TO "createdAt";--> statement-breakpoint
+ALTER TABLE "maintenance_requests" RENAME COLUMN "updated_at" TO "updatedAt";
